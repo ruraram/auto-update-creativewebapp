5b3b79f7fe24d30ae11920401a2ba22eba0c3ee5
Glitch (hello-express) <none>
2020-05-12 00:59:39 +0000

    💽🏅 Checkpoint ./public/script.js:16069387/2339

--- public/script.js
+++ public/script.js
@@ -5,21 +5,24 @@ console.log("hello world :o");
 
 // define variables that reference elements on our page
 
-
 const festsList = document.getElementById("fests");
 
 function appendNewFest(fest) {
   const newListItem = document.createElement("input");
   newListItem.src = fest[1];
-  newListItem.type="image";
-  newListItem.addEventListener("selected,getPage());
+  newListItem.type = "image";
+  newListItem.addEventListener("selected", function() {
+   
+    console.log("test");
+    //window.location.href = "page/parklife.html";
+  });
   //logo.setAttribute("src", "https://cdn.glitch.com/0eac1b7f-36d6-47d9-9e09-471cbcc4ebc9%2Fparklifelogo.jpg?v=1589236232049");
   festsList.appendChild(newListItem);
 }
-function getPage() {
-  window.open("page/parklife.html");
-  
-}
+//function getPage() {
+//window.open="page/parklife.html";
+
+//}
 fetch("/fests")
   .then(response => response.json()) // parse the JSON from the server
   .then(fests => {
@@ -27,6 +30,4 @@ fetch("/fests")
     festsList.firstElementChild.remove();
     // iterate through every dream and add it to our page
     fests.forEach(appendNewFest);
-});
-
-
+  });
